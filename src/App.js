1df48b3e5b14c313e3fae3a31f82fcedace6feb7
ElinaHordynska <additional.elina@gmail.com>
2025-11-30 15:46:16 +0200

    Quizzz

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import TestQuestion from './components/TestQuestion'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <>
+        <Header/>
+        <main>
+          <TestQuestion 
+          questionId={1}
+          questionText={"Which tag is used for styling?"}
+          answers={["h1", "style", "script", "head"]}
+          />
+        </main>
+        <Footer/>
+      </>
+    )
+  }
 }
-
-export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Header extends Component {
+  render() {
+    return (
+      <header>
+        <img src="" alt="logo" />
+        <h1>LeafQuiz</h1>
+      </header>
+    )
+  }
+}
