503ae18e800428ceb90bbb9b8b20640b6540858a
ElinaHordynska <additional.elina@gmail.com>
2025-12-07 15:38:21 +0200

    Cards

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
+import style from "../styles/components/header.module.scss"
 
 export default class Header extends Component {
   render() {
     return (
-      <header>
-        <img src="" alt="logo" />
+      <header className={style.header}>
+        <img src="./logo.png" alt="logo" />
         <h1>LeafQuiz</h1>
       </header>
     )
